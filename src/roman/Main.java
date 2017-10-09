82dade8a4ad185f8c02e156766316f0d1fc7dafe
Oskar <>
2017-10-09 21:06:09 +0200

    test, now we have just one Commit

--- src/roman/Main.java
+++ src/roman/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt");
 		System.out.println("New change in branch");
+		System.out.println("New change in branch Commit1");
+		System.out.println("New change in branch Commit2");
 	}
 
 }
